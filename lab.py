7b2ba0ca6d7036286b1f00fb1409cae57f80f5f0
Денис Усатых <den.usatykh@gmail.com>
2021-12-21 01:13:36 +0500

    Закрытие окна

--- lab.py
+++ lab.py
@@ -1,5 +1,8 @@
 import numpy as np
 import cv2
-cap = cv2.VideoCapture ('olya.mp4')
+
+cap = cv2.VideoCapture('olya.mp4')
 _, image = cap.read()
-cv2.imshow("Detection",image)
+cv2.imshow("Detection", image)
+cap.release()
+cv2.destroyAllWindows()
