60b85dab06a0e8ef232d1aca1476497e1af294e2
lunegovaOlga <lunegova_lelya@mail.ru>
2021-12-21 01:43:58 +0500

    Открытие камеры

--- lab.py
+++ lab.py
@@ -1,5 +1,10 @@
 import numpy as np
 import cv2
-cap = cv2.VideoCapture ('olya.mp4')
-_, image = cap.read()
-cv2.imshow("Detection",image)
+
+cap = cv2.VideoCapture('olya.mp4')
+while True:
+    status, image = cap.read()
+    if status:
+        cv2.imshow("Detection", image)
+cap.release()
+cv2.destroyAllWindows()
