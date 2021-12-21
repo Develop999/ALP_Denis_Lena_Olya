4870820babf6a4bc6999937583e51708f13f70b6
lunegovaOlga <lunegova_lelya@mail.ru>
2021-12-21 14:10:03 +0500

    Ширина, высота, количеств каналов изображения

--- lab.py
+++ lab.py
@@ -4,6 +4,8 @@ import cv2
 cap = cv2.VideoCapture('olya.mp4')
 while True:
     status, image = cap.read()
+    H, W, chanels = image.shape
+    print(H, W, chanels)
     if not status:
         break
     cv2.imshow("Detection", image)
