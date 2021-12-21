f505ee8f7fd3c54ad11d9978eae99c44c8dd7f5d
Elena_Bobrova <l.bobrova09@gmail.com>
2021-12-21 09:51:40 +0500

    добавление закрытия окна

--- lab.py
+++ lab.py
@@ -4,7 +4,11 @@ import cv2
 cap = cv2.VideoCapture('olya.mp4')
 while True:
     status, image = cap.read()
-    if status:
-        cv2.imshow("Detection", image)
+    if not status:
+        break
+    cv2.imshow("Detection", image)
+    if cv2.waitKey(0):
+        break
+
 cap.release()
 cv2.destroyAllWindows()
