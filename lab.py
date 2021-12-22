f25e630d50ec11ab4f8fc01c5300cc16fc7e3657
Elena_Bobrova <l.bobrova09@gmail.com>
2021-12-22 21:56:50 +0500

    вывод прямоугольников

--- lab.py
+++ lab.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture('lena.mp4')
+cap = cv2.VideoCapture('olya.mp4')
 classes_path = "coco.names"
 classes = open(classes_path).read().strip().split("\n")
 weights_path = "yolov4-tiny.weights"
@@ -15,7 +15,7 @@ while True:
     if not status:
         break
     H, W, _ = image.shape
-    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (256, 256), swapRB=True, crop=False)
+    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (320, 320), swapRB=True, crop=False)
     model.setInput(blob)
     layerOutputs = model.forward(layer_name)
     results = []
@@ -37,6 +37,8 @@ while True:
         x, y, w, h = boxes[i]
         res = (confidences[i], (x, y), (x + w, y + h))
         results.append(res)
+    for res in results:
+        cv2.rectangle(image, res[1], res[2], (0, 255, 0), 2)
 
     cv2.imshow("Detection", image)
     if cv2.waitKey(1) == 27:
