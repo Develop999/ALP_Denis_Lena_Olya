14803d38d038c4b89bf52ad7bd1f5fd271e92143
lunegovaOlga <lunegova_lelya@mail.ru>
2021-12-22 23:23:05 +0500

    оптимизация

--- lab.py
+++ lab.py
@@ -1,19 +1,10 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture('olya.mp4')
-classes_path = "coco.names"
-classes = open(classes_path).read().strip().split("\n")
-weights_path = "yolov4-tiny.weights"
-config_path = "yolov4-tiny.cfg"
-model = cv2.dnn.readNet(config_path, weights_path)
-layer_name = model.getLayerNames()
-layer_name = [layer_name[i - 1] for i in model.getUnconnectedOutLayers()]
 
-
-def pedestrian_detection(img, model, layer_name, id):
+def pedestrian_detection(img, model, layer_name, personid):
     H, W, _ = img.shape
-    blob = cv2.dnn.blobFromImage(img, 1 / 255.0, (320, 320), swapRB=True, crop=False)
+    blob = cv2.dnn.blobFromImage(img, 1 / 255.0, (416, 416), swapRB=True, crop=False)
     model.setInput(blob)
     layerOutputs = model.forward(layer_name)
     results = []
@@ -30,19 +21,29 @@ def pedestrian_detection(img, model, layer_name, id):
             y = int(centerY - (height / 2))
             boxes.append([x, y, int(width), int(height)])
             confidences.append(float(confidence))
-    idzs = cv2.dnn.NMSBoxes(boxes, confidences, 0.7, 0.8)
-    for i in idzs:
-        x, y, w, h = boxes[i]
-        res = (confidences[i], (x, y), (x + w, y + h))
-        results.append(res)
+    idzs = cv2.dnn.NMSBoxes(boxes, confidences, 0.25, 0.6)
+    if len(idzs):
+        for i in idzs:
+            x, y, w, h = boxes[i]
+            res = (confidences[i], (x, y), (x + w, y + h))
+            results.append(res)
     return results
 
+
+cap = cv2.VideoCapture('olya.mp4')
+classes_path = "coco.names"
+classes = open(classes_path).read().strip().split("\n")
+weights_path = "yolov4-tiny.weights"
+config_path = "yolov4-tiny.cfg"
+model = cv2.dnn.readNet(config_path, weights_path)
+layer_name = model.getLayerNames()
+layer_name = [layer_name[i - 1] for i in model.getUnconnectedOutLayers()]
 
 while True:
     status, image = cap.read()
     if not status:
         break
-    results = pedestrian_detection(image, model, layer_name, 0)
+    results = pedestrian_detection(image, model, layer_name, classes.index("person"))
     for res in results:
         cv2.rectangle(image, res[1], res[2], (0, 255, 0), 2)
     cv2.imshow("Detection", image)
