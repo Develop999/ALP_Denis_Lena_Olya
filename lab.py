c1438c4006a011e02a7d5b92e1c1820202cab11b
Elena_Bobrova <l.bobrova09@gmail.com>
2021-12-24 22:33:12 +0500

    добавление функции пересечения прямоугольников

--- lab.py
+++ lab.py
@@ -6,6 +6,26 @@ import time
 NMS_THRESHOLD = 0.3
 MIN_CONFIDENCE = 0.7
 
+def check_intersection(el, rect):
+    for (ax1, ay1, ax2, ay2) in rect:
+        break
+    ax2 += ax1
+    ay2 += ay1
+
+    for (bx1, by1, bx2, by2) in rect:
+        break
+    bx2 += bx1
+    by2 += by1
+
+    s1 = (ax1 >= bx1 and ax1 <=bx2) or (ax2 >= bx1 and ax2 <= bx2)
+    s2 = (ay1 >= by1 and ay1 <= by2) or (ay2 >= by1 and ay2 <= by2)
+    s3 = (bx1 >= ax1 and bx1 <= ax2) or (bx2 >= ax1 and bx2 <= ax2)
+    s4 = (ax1 >= bx1 and ax1 <= bx2) or (by2 >= ay1 and by2 <= ay2)
+
+    if ((s1 and s2) or (s3 and s4)) or ((s1 and s4) or (s3 and s2)):
+        return True
+    else:
+        return False
 
 def pedestrian_detection(img, model, layer_name, personid):
     H, W, _ = img.shape
