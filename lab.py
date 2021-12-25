b12925a01dbc0ce2aa2a5faa011fbbca6972de2f
Денис Усатых <den.usatykh@gmail.com>
2021-12-25 18:34:29 +0500

    Update

--- lab.py
+++ lab.py
@@ -6,6 +6,7 @@ NMS_THRESHOLD = 0.25
 MIN_CONFIDENCE = 0.5
 zona = np.array([[200, 300, 500, 600]])
 
+
 def check_intersection(body, rect):
     for (ax1, ay1, ax2, ay2) in body:
         break
@@ -29,6 +30,7 @@ def pedestrian_detection(img, model, layer_name, personid):
     results = []
     boxes = []
     confidences = []
+    labels = []
     blob = cv2.dnn.blobFromImage(img, 0.00392, (320, 320), swapRB=True, crop=False)
     model.setInput(blob)
     layerOutputs = model.forward(layer_name)
@@ -44,14 +46,16 @@ def pedestrian_detection(img, model, layer_name, personid):
                 y = int(centerY - (height / 2))
                 boxes.append([x, y, int(width), int(height)])
                 confidences.append(float(confidence))
+                labels.append(classID)
 
     idzs = cv2.dnn.NMSBoxes(boxes, confidences, MIN_CONFIDENCE, NMS_THRESHOLD)
     if len(idzs):
         for i in idzs:
             x, y, w, h = boxes[i]
-            res = (confidences[i], (x, y), (x + w, y + h))
+            res = (confidences[i], (x, y), (x + w, y + h), labels[i])
             results.append(res)
     return results
+
 
 classes_path = "coco.names"
 classes = open(classes_path).read().strip().split("\n")
@@ -59,15 +63,16 @@ weights_path = "yolov4-tiny.weights"
 config_path = "yolov4-tiny.cfg"
 
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('captured.mp4', fourcc, 24, (1280, 720))
-
-cap = cv2.VideoCapture('olya.mp4')
+out = cv2.VideoWriter('captured4.mp4', fourcc, 30, (1280, 720))
 
 model = cv2.dnn.readNet(config_path, weights_path)
 layer_name = model.getLayerNames()
 layer_name = [layer_name[i - 1] for i in model.getUnconnectedOutLayers()]
 
 cap = cv2.VideoCapture('olya.mp4')
+font = cv2.FONT_HERSHEY_SIMPLEX
+green = (0, 255, 0)
+red = (0, 0, 255)
 
 while True:
     start = time.time()
@@ -78,18 +83,21 @@ while True:
     results = pedestrian_detection(image, model, layer_name, classes.index("person"))
     for (x1, y1, x2, y2) in zona:
         cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 2)
-    for res in results:
+    for i, res in enumerate(results, start=1):
         if len(results):
             x1, y1 = res[1]
             x2, y2 = res[2]
+            label = classes[res[3]]
             if check_intersection([res[1] + res[2]], zona):
-                cv2.rectangle(image, res[1], res[2], (0, 0, 255), 2)
+                cv2.putText(image, '{}.{} {:.0%}'.format(i, label, res[0]), (x1 + 2, y1 - 5), font, 0.35, green, 1)
+                cv2.rectangle(image, res[1], res[2], red, 2)
             else:
-                cv2.rectangle(image, res[1], res[2], (0, 255, 0), 2)
+                cv2.putText(image, '{}.{} {:.0%}'.format(i, label, res[0]), (x1 + 2, y1 - 5), font, 0.35, green, 1)
+                cv2.rectangle(image, res[1], res[2], green, 2)
     seconds = time.time() - start
     fps = 1 / seconds
     fps = round(fps, 2)
-    cv2.putText(image, str(fps), (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 0), 2, cv2.LINE_AA)
+    cv2.putText(image, str(fps), (5, 30), font, 1, (100, 255, 0), 2)
     cv2.imshow("Detection", image)
     out.write(image)
     if cv2.waitKey(1) == 27:
